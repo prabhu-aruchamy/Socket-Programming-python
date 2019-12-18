97d89850bc94cb411af7d519a273cf882df67f8e
Prabhu <prabhuprashanth838@gmail.com>
2019-12-18 23:09:34 +0530

    factorial

--- factorial_client.py
+++ factorial_client.py
@@ -5,4 +5,8 @@ PORT = 2500
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    number = int(input("Enter a number that you want to find factorial"))
+    number = input("Enter a number that you want to find factorial: ")
+    number = bytes(number, 'UTF-8')
+    s.sendall(number)
+    res = s.recv(3000)
+    print("The Factorial of the number", number.decode('UTF-8'), "is", res.decode('UTF-8'))
---
+++ factorial_server.py
@@ -0,0 +1,32 @@
+import socket
+import math
+
+# def facto(number):
+#     number = int(number.decode('UTF-8'))
+#     factorial(number)
+#     return number
+
+#main
+
+
+HOST = 'localhost'
+PORT = 2500
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print("Server Listening at port", PORT)
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print("Connected in", addr)
+            while True:
+                num = conn.recv(3000)
+                if not num:
+                    break
+                num = num.decode('UTF-8')
+                res = math.factorial(int(num))
+                print("res= ", res)
+                res = bytes(str(res), 'UTF-8')
+                conn.sendall(res)
+
+
