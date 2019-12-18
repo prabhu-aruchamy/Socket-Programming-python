d67d0326044c387497d4ff2fe4618e50c91c0dab
Prabhu <prabhuprashanth838@gmail.com>
2019-12-18 21:49:56 +0530

    echo

---
+++ echo-client.py
@@ -0,0 +1,13 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        msg = input("Enter Message: ")
+        fmsg = bytes(msg, 'UTF-8')
+        s.sendall(fmsg)
+        data = s.recv(1024)
+        print('[CLIENT] Received back from server', data.decode('UTF-8'))
---
+++ echo-server.py
@@ -0,0 +1,18 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print("Server Listening...")
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print("[SERVER] The data received from client is", data.decode('UTF-8'))
+            conn.sendall(data)
